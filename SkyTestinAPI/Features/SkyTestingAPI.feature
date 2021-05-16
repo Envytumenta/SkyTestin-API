b8c9b0be65f052dee498cd74660bb8b522807b36
emmanuel <user@DESKTOP-U1MM9OL>
2021-05-16 15:37:01 +0100

    Add project files.

---
+++ SkyTestinAPI/Features/SkyTestingAPI.feature
@@ -0,0 +1,18 @@
+﻿Feature: SkyTestingAPI
+	
+
+@GET
+Scenario: List of Articles
+	Given I have access to the endpoint
+	When I search for the endpoint
+	Then result is displayed
+
+	@GET
+Scenario: Single Article
+	Given I have access to the single endpoint
+	When I search for the endpoint
+	Then result is displayed
+
+
+
+
---
+++ SkyTestinAPI/Steps/SkyTestingAPISteps.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using RestSharp;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SkyTestinAPI.Steps
+{
+    
+
+    [Binding]
+    public class SkyTestingAPISteps
+    {
+        RestClient client;
+        RestRequest request;
+        IRestResponse response;
+
+            [Given(@"I have access to the endpoint")]
+        public void GivenIHaveAccessToTheEndpoint()
+        {
+            client = new RestClient("https://5f99522350d84900163b8737.mockapi.io/tech-test/articles");
+            client.Timeout = -1;
+            
+        }
+        
+        [When(@"I search for the endpoint")]
+        public void WhenISearchForTheEndpoint()
+        {
+            request = new RestRequest(Method.GET);
+            response = client.Execute(request);
+            Console.WriteLine(response.Content);
+        }
+        
+        [Then(@"result is displayed")]
+        public void ThenResultIsDisplayed()
+        {
+            Assert.That(response.Content.Length > 0);
+            Assert.That(response.StatusCode.ToString() == "OK");
+            Assert.That(response.IsSuccessful.ToString() == "True");
+        }
+
+        [Given(@"I have access to the single endpoint")]
+        public void GivenIHaveAccessToTheSingleEndpoint()
+        {
+            client = new RestClient("https://5f99522350d84900163b8737.mockapi.io/tech-test/articles/2");
+            client.Timeout = -1;
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
